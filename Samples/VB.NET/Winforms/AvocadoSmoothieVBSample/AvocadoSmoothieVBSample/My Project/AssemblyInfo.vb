0b877bac9232ac6813bfd9702e073038b7091ca8
Charlie Jaewoong Mun <happybono@outlook.com>
2025-12-30 14:39:22 +0900

    Add files via upload

---
+++ Samples/VB.NET/Winforms/AvocadoSmoothieVBSample/AvocadoSmoothieVBSample/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("AvocadoSmoothieVBSample")>
+<Assembly: AssemblyDescription("A companion tasting room study application for AvocadoSmoothie.Barista - experiment with running‑median smoothing and CSV / Excel export workflows before serving your own production-ready applications.")>
+<Assembly: AssemblyCompany("Codies")>
+<Assembly: AssemblyProduct("AvocadoSmoothieVBSample")>
+<Assembly: AssemblyCopyright("Copyright © HappyBono 2025")>
+<Assembly: AssemblyTrademark("AvocadoSmoothie")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8b28008f-e25e-4bf1-a035-76c9ecf1eab1")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
